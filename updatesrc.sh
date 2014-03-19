d09f1480c6a4f34fba8010fefc73190d5900d22d
Justin Cormack <justin@specialbusservice.com>
2014-03-19 00:10:50 +0000

    Add sbin/newfs_v7fs needed by mkfs

--- updatesrc.sh
+++ updatesrc.sh
@@ -41,15 +41,17 @@ NBSRC_EXTRA=''
 files () { pfx=$1; shift; for arg in $* ; do echo src/${pfx}${arg} ; done }
 
 lsfiles () {
-	files lib/lib		c crypt ipsec m npf pci prop pthread rmt util y z
+	files lib/lib		c crypt ipsec m npf pci prop
+	files lib/lib		pthread rmt util y z
 	files libexec/		ld.elf_so
 
 	files bin/		cat cp dd df ln ls mkdir mv pax rm rmdir
 
 	files sbin/		cgdconfig
 	files sbin/		disklabel dump fsck fsck_ffs ifconfig mknod
-	files sbin/		modstat mount mount_ffs newfs ping ping6
-	files sbin/		raidctl reboot rndctl route setkey sysctl umount
+	files sbin/		modstat mount mount_ffs newfs newfs_v7fs
+	files sbin/		ping ping6 raidctl reboot
+	files sbin/		rndctl route setkey sysctl umount
 
 	files usr.bin/		kdump ktrace
 	files usr.sbin/		arp dumpfs makefs ndp npf pcictl vnconfig
