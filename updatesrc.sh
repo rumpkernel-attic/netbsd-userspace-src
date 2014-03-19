0b05ead1841a7977392ecd6b94ccf1f505e2f6a0
Justin Cormack <justin@specialbusservice.com>
2014-03-19 00:44:49 +0000

    newfs_* requires fsck_* as well

--- updatesrc.sh
+++ updatesrc.sh
@@ -48,7 +48,9 @@ lsfiles () {
 	files bin/		cat cp dd df ln ls mkdir mv pax rm rmdir
 
 	files sbin/		cgdconfig
-	files sbin/		disklabel dump fsck fsck_ffs ifconfig mknod
+	files sbin/		disklabel dump fsck fsck_ext2fs fsck_ffs
+	files sbin/		fsck_lfs fsck_msdos fsck_v7fs
+	files sbin/		ifconfig mknod
 	files sbin/		modstat mount mount_ffs newfs
 	files sbin/		newfs_v7fs newfs_msdos newfs_ext2fs
 	files sbin/		newfs_lfs newfs_sysvbfs newfs_udf
